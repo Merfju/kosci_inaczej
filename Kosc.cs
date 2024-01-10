6101d7691705a79637d989da7882f5c89239d979
Merfju <mateusz.m06@wp.pl>
2024-01-10 16:05:55 +0100

    blokowanie kosci

--- Kosc.cs
+++ Kosc.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfAppKosci
 {
-    public class Kosc
+    public class Kosc : INotifyPropertyChanged
     {
-        public int Wartosc { get; set; }
+        private int _wartosc;
+        public int Wartosc { 
+            get => _wartosc;
+            set 
+            {
+                _wartosc = value;
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Wartosc)));
+                    //możemy obserować wartość na kostce
+                }
+            } 
+        }
         public bool CzyZaznaczona { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfAppKosci
 {
@@ -21,13 +10,14 @@ namespace WpfAppKosci
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<int> rezultaty { get; set; }
+        public ObservableCollection<Kosc> rezultaty { get; set; }
+        public int LiczbaKosci { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            rezultaty = new ObservableCollection<int>();
+            rezultaty = new ObservableCollection<Kosc>();
             DataContext = this; // żeby działało bindowanie do XAML
-
+            LiczbaKosci = 5;
 
 
 
@@ -35,21 +25,37 @@ namespace WpfAppKosci
 
         private void rzuc_btn_Click(object sender, RoutedEventArgs e)
         {
-            rezultaty.Clear();
-            Random random = new Random();
-            for(int i=0; i<10; i++)
+            if (rezultaty.Count == 0)
             {
-                rezultaty.Add(random.Next(1,7));
+                for (int i = 0; i < LiczbaKosci; i++)
+                {
+                    rezultaty.Add(new Kosc());
+                }
+            }
+
+
+            Random random = new Random();
+            foreach (Kosc k in rezultaty)
+            {
+                if (k.CzyZaznaczona == false)
+                    k.Wartosc = random.Next(1, 7);
             }
         }
 
         private void wyczysc_btn_Click(object sender, RoutedEventArgs e)
         {
             rezultaty.Clear();
-            for(int i=0; i<10; i++)
+            for (int i = 0; i < LiczbaKosci; i++)
             {
-                rezultaty.Add(0);
+                rezultaty.Add(new Kosc());
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Kosc kosc = (Kosc)button.DataContext;
+            kosc.CzyZaznaczona = !kosc.CzyZaznaczona;
         }
     }
 }
